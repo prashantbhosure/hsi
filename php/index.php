918ab72d89a9dd73541bc46a28d155d0eadd7fdc
prashantbhosure <prashant.bhosure@gmail.com>
2016-12-28 12:49:37 +0530

    Update index.php

--- php/index.php
+++ php/index.php
@@ -301,3 +301,17 @@ $ git push</pre>
 
 </body>
 </html>
+
+<? Php
+echo $ Host = getenv ('OPENSHIFT_MYSQL_DB_HOST');
+echo $ Port = getenv ('OPENSHIFT_MYSQL_DB_PORT');
+echo $ User = getenv ('OPENSHIFT_MYSQL_DB_USERNAME');
+echo $ Pass = getenv ('OPENSHIFT_MYSQL_DB_PASSWORD');
+echo $ Addr = $ host. ":". $ Port; // always include the door
+
+echo $ Link = mysql_connect ($ addr, $ user, $ pass);
+
+echo $ Name = getenv ('OPENSHIFT_APP_NAME'); // default database
+
+//Mysql_select_db ($ name);
+?>
