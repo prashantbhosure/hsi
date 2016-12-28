bc3026ddf43165a285061dd0cf8f29cd78780617
prashantbhosure <prashant.bhosure@gmail.com>
2016-12-28 12:45:52 +0530

    Create test.php

---
+++ php/test.php
@@ -0,0 +1,13 @@
+<? Php
+$ Host = getenv ('OPENSHIFT_MYSQL_DB_HOST');
+$ Port = getenv ('OPENSHIFT_MYSQL_DB_PORT');
+$ User = getenv ('OPENSHIFT_MYSQL_DB_USERNAME');
+$ Pass = getenv ('OPENSHIFT_MYSQL_DB_PASSWORD');
+$ Addr = $ host. ":". $ Port; // always include the door
+
+$ Link = mysql_connect ($ addr, $ user, $ pass);
+
+$ Name = getenv ('OPENSHIFT_APP_NAME'); // default database
+
+Mysql_select_db ($ name);
+?>
