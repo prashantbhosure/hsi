d7c5d24abd295b612d57e852e3e26ee25147eff0
prashantbhosure <prashant.bhosure@gmail.com>
2016-12-28 15:47:38 +0530

    Update test.php

--- php/test.php
+++ php/test.php
@@ -1,6 +1,7 @@
 <?php
 $host = getenv('OPENSHIFT_MYSQL_DB_HOST');
 echo "hi33";
+echo getenv('OPENSHIFT_MYSQL_DB_HOST');
 $port = getenv('OPENSHIFT_MYSQL_DB_PORT');
 $user = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 $pass = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
