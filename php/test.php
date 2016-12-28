321b00437a912d8cd8e8fefc606092373d299fc6
prashantbhosure <prashant.bhosure@gmail.com>
2016-12-28 15:16:20 +0530

    Update test.php

--- php/test.php
+++ php/test.php
@@ -1,13 +1,12 @@
 <?php
 $host = getenv('OPENSHIFT_MYSQL_DB_HOST');
-echo $host;
-echo "hi";
+echo "hi33";
 $port = getenv('OPENSHIFT_MYSQL_DB_PORT');
 $user = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 $pass = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 $addr = $host . ":" . $port; //inclua sempre a porta
 
-$link = mysqli_connect($addr, $user, $pass);
+$link = mysqli_connect($addr, $user, $pass, $name);
 
 $name = getenv('OPENSHIFT_APP_NAME'); //base de dados padrão
 
