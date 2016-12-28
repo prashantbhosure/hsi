18ad49e9b4f779183d8623d00d755ca0bf1f8071
prashantbhosure <prashant.bhosure@gmail.com>
2016-12-28 13:23:37 +0530

    Update test.php

--- php/test.php
+++ php/test.php
@@ -1,13 +1,13 @@
 <? Php
-echo $ Host = getenv ('OPENSHIFT_MYSQL_DB_HOST');
-echo $ Port = getenv ('OPENSHIFT_MYSQL_DB_PORT');
-echo $ User = getenv ('OPENSHIFT_MYSQL_DB_USERNAME');
-echo $ Pass = getenv ('OPENSHIFT_MYSQL_DB_PASSWORD');
-echo $ Addr = $ host. ":". $ Port; // always include the door
+$ Host = getenv ('OPENSHIFT_MYSQL_DB_HOST');
+$ Port = getenv ('OPENSHIFT_MYSQL_DB_PORT');
+$ User = getenv ('OPENSHIFT_MYSQL_DB_USERNAME');
+$ Pass = getenv ('OPENSHIFT_MYSQL_DB_PASSWORD');
+$ Addr = $ host. ":". $ Port; // always include the door
 
-//$ Link = mysql_connect ($ addr, $ user, $ pass);
+$ Link = mysql_connect ($ addr, $ user, $ pass);
 
-echo $ Name = getenv ('OPENSHIFT_APP_NAME'); // default database
+$ Name = getenv ('OPENSHIFT_APP_NAME'); // default database
 
-//Mysql_select_db ($ name);
+Mysql_select_db ($ name);
 ?>
